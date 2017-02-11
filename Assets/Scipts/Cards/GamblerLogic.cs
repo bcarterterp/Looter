271adb12f8d8e788c646ab3412115c7ff5035f00
bcarterterp <bmcarter89@gmail.com>
2017-02-10 21:39:56 -0500

    added newly created files

---
+++ Assets/Scipts/Cards/GamblerLogic.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamblerLogic : CardLogic{
+
+	private bool guessedCorrectly;
+	private int gold;
+
+	public override void Interact (Hero hero, int option)
+	{
+		PlayShellGame (hero, option);
+	}
+
+	private void PlayShellGame(Hero hero, int option){
+		guessedCorrectly = Random.Range (0, 3) == option;
+		if (guessedCorrectly) {
+			gold = 50 * hero.getLevel ();
+		} else {
+			gold = -50;
+		}
+		hero.AdjustGold (gold);
+		ShouldProgress ();
+	}
+
+	public override string GetStoryText()
+	{
+		switch (GetStage ()) {
+		case 0:
+			return GetDiscoveryText ();
+		case 1:
+			return GetSelectOptionText ();
+		case 2:
+			return GetResultsText();
+		default:
+			return "WHAT!?!?!";
+		}
+	}
+
+	private string GetDiscoveryText(){
+		ShouldProgress ();
+		return "Hello traveler, mlet's play a gmae of shells!";
+	}
+
+	private string GetSelectOptionText(){
+		ShouldProgress ();
+		return "Select one, may luck be on your side...";
+	}
+
+	private string GetResultsText(){
+		if (guessedCorrectly) {
+			return "Lady Luck has smiled upon you! You one" + gold + "gold";
+		} else {
+			return "Maybe next time traveler!";
+		}
+	}
+
+	public override bool IsLastStage ()
+	{
+		return GetStage () == 2;
+	}
+
+}
---
+++ Assets/Scipts/Cards/PasserbyLogic.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PasserbyLogic : CardLogic {
+
+	private AdventureStack stack;
+	private int numberOfMonsters;
+
+	public PasserbyLogic(AdventureStack adventureStack){
+		stack = adventureStack;
+	}
+
+	public override void Interact (Hero hero)
+	{
+		if (GetStage () == 1) {
+			PopulateAdventureStack ();
+		} else {
+			ShouldProgress ();
+		}
+	}
+
+	public void PopulateAdventureStack(){
+		stack.Push (CardType.RESCUED);
+		numberOfMonsters = Random.Range (0, 4);
+		for (int i = 0; i < numberOfMonsters; i++) {
+			stack.Push (CardType.MONSTER);
+		}
+	}
+
+	public override string GetStoryText ()
+	{
+		switch (GetStage ()) {
+		case 0:
+			return GetDiscoveryText ();
+		case 1:
+			return GetAcceptedText ();
+		default:
+			return "What!?!?!";
+		}
+	}
+
+	private string GetDiscoveryText(){
+		return "Help! My father has been kidnapped, will you help bring him back?";
+	}
+
+	private string GetAcceptedText(){
+		return "Thank you so much! Please bring him back safely"; 
+	}
+}
